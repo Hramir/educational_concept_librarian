7fbc77eeaaf0ecce91c17c57bcf18ddc5349ce02
rhern <raulraulzinho@yahoo.com>
2023-11-13 15:33:11 -0500

    Wrote tests for the store_playlist_videos_metadata function

--- data_scraper/test_data_scraper.py
+++ data_scraper/test_data_scraper.py
@@ -1,5 +1,5 @@
 from playlist_data_scraper import video_ids_from_playlist, video_metadata_from_id
-from playlist_data_scraper import video_transcript_from_id
+from playlist_data_scraper import video_transcript_from_id, store_playlist_videos_metadata
 
 # Tests the video_ids_from_playlist function
 video_ids = video_ids_from_playlist("PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG")
@@ -36,5 +36,19 @@ for word in transcript:
         assert char not in word, f"Expected no whitespace in words, but found whitespace in {word}."
 assert transcript[0] == "Hey", "Expected first word in transcript to be \"Hey\""
 assert transcript[-1] == "want.", "Expected last word in transcript to be \"want.\""
+
+
+
+# Tests the store_playlist_video_metadata function
+store_playlist_videos_metadata("PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG", "data_scraper/")
+with open("data_scraper/PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG 0.txt", "r") as file:
+    assert file.readline()[:-1] == "KswZ1wnCejs", "Expected correct video ID."
+    assert file.readline()[:-1] == "239", "Expected correct video duration."
+    assert int(file.readline()[:-1]) >= 99, "Expected correct view count."
+    assert file.readline()[:-1] == "UC2YE5AUXk8KCGPLOfY9eCXA", "Expected correct channel ID."
+# Manual Testing Strategy:
+# Check lines after the metadata tested above to see if they match the transcript
+# from the video
+
 
 print("All tests passed!")
