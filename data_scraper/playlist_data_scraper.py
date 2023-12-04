8240b5d0f3ce0d3f3a820191dd3f1d994edfd0f9
rhern <raulraulzinho@yahoo.com>
2023-12-04 15:48:31 -0500

    Added more metadata for videos, restructured the video data file format, and added data about video's comments

--- data_scraper/playlist_data_scraper.py
+++ data_scraper/playlist_data_scraper.py
@@ -1,4 +1,4 @@
-from youtubesearchpython import Playlist, Video
+from youtubesearchpython import Channel, Playlist, Video
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def video_ids_from_playlist(playlist_id):
@@ -28,13 +28,15 @@ def video_ids_from_playlist(playlist_id):
     return video_ids
         
 
-def video_metadata_from_id(video_id):
+def video_metadata_from_id(video_id, youtube_client):
     """
     Downloads and returns difference pieces of metadata about the video associated
     with the given ID.
 
     video_id: str
         The ID of the video whose metadata will be extracted.
+    youtube_client: Resource
+        The YouTube API Client built using an API key.
 
     Returns: dict mapping str to str
         A dictionary that maps a metadata label to the corresponding metadata.
@@ -43,24 +45,34 @@ def video_metadata_from_id(video_id):
         instead, keep all old keys and only ever add new keys.
 
         Currently, the keys in the dict are:
+            title: The title of the video
             id: The ID of the video
             duration: The number of seconds the video lasts
             views: The number of views the video has
-            channel: The ID of the channel that posted the video
-            title: The title of the video
+            likes: The number of likes the video has
+            channel_name: The name of the channel that posted the video
+            channel_id: The ID of the channel that posted the video
     """
 
-    # Loads the video metadata
+    # Loads the video metadata (excluding like count)
     video_info = Video.getInfo(video_id)
 
+    # Prepares a request for extracting video likes
+    request = youtube_client.videos().list(
+        part='statistics',
+        id=video_id
+    )
+    response = request.execute()
+
     return {
+        "title": video_info["title"],
         "id": video_info["id"],
         "duration": video_info["duration"]["secondsText"],
         "views": video_info["viewCount"]["text"],
-        "channel": video_info["channel"]["id"],
-        "title": video_info["title"],
+        "likes": response['items'][0]['statistics']['likeCount'],
+        "channel_name": video_info["channel"]["name"],
+        "channel_id": video_info["channel"]["id"],
     }
-
 
 def video_transcript_from_id(video_id):
     """
@@ -70,14 +82,14 @@ def video_transcript_from_id(video_id):
     video_id: str
         The ID of the video whose transcript will be downloaded.
 
-    Returns: list of str
+    Returns: str
         The words of the video's transcribed text, in the order they were transcribed.
-        May include strings that are not English words (e.g., mathematical expressions).
-        Words are split wherever a whitespace character appears in the YouTube transcript.
-        Punctuation used is still kept in the word string.
+        May include substrings that are not English words (e.g., mathematical expressions).
+        Words are split and "glued" together with a space character wherever a whitespace
+        character appears in the original YouTube transcript. Punctuation used is still kept
+        in the word string.
 
-        If the video lacks a transcript, a one-element list ["NO TRANSCRIPT AVAILABLE"]
-        is returned instead.
+        If the video lacks a transcript, the string "NO TRANSCRIPT AVAILABLE" is returned instead.
     """
     
     # Downloads the transcript data
@@ -85,17 +97,72 @@ def video_transcript_from_id(video_id):
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=("en", "en-US"))
     except:
         print("No transcript found for", video_id)
-        return ["NO TRANSCRIPT AVAILABLE"]
+        return "NO TRANSCRIPT AVAILABLE"
 
     # Stores each individual word in the transcript
     words = []
     for line in transcript:
         words += line["text"].split()
 
-    return words
+    # Glues words together with a space character between them
+    complete_transcript = words[0]
+    for word in words[1:]:
+        complete_transcript += " " + word
+    return complete_transcript
+
+def video_comments_from_id(video_id, youtube_client):
+    """
+    Downloads and returns the top-level comments on the video with the given video ID.
+
+    video_id: str
+        The ID of the video whose comments will be downloaded.
+    youtube_client: Resource
+        The YouTube API Client built using an API key.
+
+    Returns: list of str
+        The comments on the video with the given video ID, where all newline characters have
+        been replaced with space characters.
+    """
+
+    # Stores values for the function
+    comments = []
+    nextPageToken = None
+
+    # Goes through each page of comments
+    while True:
+
+        # Requests the data of the video
+        response = youtube_client.commentThreads().list(
+            part='snippet',
+            videoId=video_id,
+            textFormat='plainText',
+            pageToken=nextPageToken
+        ).execute()
+
+        # Runs through each comment
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+
+            # Replaces newlines in the comments with spaces
+            mod_comment = ""
+            for comment_char in comment.strip():
+                if comment_char.isascii():
+                    if comment_char == "\n":
+                        mod_comment += " "
+                    else:
+                        mod_comment += comment_char
+
+            comments.append(mod_comment)
+
+        # Goes to the next page of comments, if any
+        nextPageToken = response.get('nextPageToken')
+        if not nextPageToken:
+            break
+
+    return comments
 
 
-def store_playlist_videos_metadata(playlist_id, local_path_str=""):
+def store_playlist_videos_metadata(playlist_id, youtube_client, local_path_str=""):
     """
     Stores the metadata (including transcript) from each video in the given playlist
     at the path provided in a text file named:
@@ -103,21 +170,24 @@ def store_playlist_videos_metadata(playlist_id, local_path_str=""):
 
     Below is the format of each video metadata file:
 
-    Line 1: Video ID
-    Line 2: Video duration in seconds
-    Line 3: The number of views the video has as of downloading the data
-    Line 4: Channel ID
-    Line 5: Video Title
-
-    Lines 6+: The words (split as in video_transcript_from_id) from the video's
-    transcript, each on its own line.
-
-    The number of lines from the beginning of the file to the point at which the
-    transcribed words begin is equal to the number of keys in the dictionary returned by
-    the video_metadata_from_id function.
+    Line 1: Playlist ID
+    Line 2: Position (0-indexed) of the video in the playlist
+    Line 3: Total number of videos in the playlist from which the video came
+    Line 4: Video title
+    Line 5: Video ID
+    Line 6: Video duration in seconds
+    Line 7: Number of views the video has, as of downloading the data
+    Line 8: Number of likes the video has, as of downloading the data
+    Line 9: Channel name
+    Line 10: Channel ID
+    Line 11: Transcript
+    Lines 12+: Top-level comments on the video, where each line corresponds to an
+    individual top-level comment on the video. The replies to comments are not included.
 
     playlist_id: str
         The ID of the playlist from whom the videos' metadata will be stored.
+    youtube_client: Resource
+        The YouTube API Client built using an API key.
     local_path_str: str
         The local path to the directory at which the files will be stored
         (without "/" at the end), or "" if the root directory.
@@ -128,20 +198,32 @@ def store_playlist_videos_metadata(playlist_id, local_path_str=""):
         local_path_str = local_path_str[:-1]
 
     # Runs through each video in the playlist
-    for index, video_id in enumerate(video_ids_from_playlist(playlist_id)):
+    video_ids = video_ids_from_playlist(playlist_id)
+    for index, video_id in enumerate(video_ids):
 
         # Stores the info from the video
-        metadata = video_metadata_from_id(video_id)
+        metadata = video_metadata_from_id(video_id, youtube_client)
         transcript = video_transcript_from_id(video_id)
+        comments = video_comments_from_id(video_id, youtube_client)
 
         # Writes the video data as specified
-        with open(f"{local_path_str}{('/' if local_path_str!='' else '')}{playlist_id} {index}.txt", "w") as file:
+        with open(f"{local_path_str}{('/' if local_path_str!='' else '')}{playlist_id} {str(index).zfill(3)}.txt", "w") as file:
 
-            # Stores the non-transcript metadata
-            for tag in ["id", "duration", "views", "channel", "title"]:
+            # Stores the playlist ID
+            file.write(playlist_id + "\n")
+
+            # Stores the video's playlist position/length
+            file.write(str(index) + "\n")
+            file.write(str(len(video_ids)) + "\n")
+
+            # Stores the non-transcript/comment metadata
+            for tag in ["title", "id", "duration", "views", "likes", "channel_name", "channel_id"]:
                 file.write(metadata[tag] + "\n")
 
             # Stores the video's transcribed words
-            for word in transcript:
-                file.write(word + "\n")
+            file.write(transcript + "\n")
+
+            # Stores the video's top-level comments
+            for comment in comments:
+                file.write(comment + "\n")
         
--- data_scraper/test_data_scraper.py
+++ data_scraper/test_data_scraper.py
@@ -1,5 +1,10 @@
 from playlist_data_scraper import video_ids_from_playlist, video_metadata_from_id
 from playlist_data_scraper import video_transcript_from_id, store_playlist_videos_metadata
+from googleapiclient.discovery import build
+
+# Creates the YouTube API client
+API_KEY = "AIzaSyBt5u5kxlrxC7qu7oT10073aIttAOM00pE"
+YOUTUBE_CLIENT = build('youtube', 'v3', developerKey=API_KEY)
 
 # Tests the video_ids_from_playlist function
 video_ids = video_ids_from_playlist("PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG")
@@ -13,7 +18,11 @@ assert actual == expected, f"Expected {expected}, but got {actual}."
 #
 # When a new key is added to the output as per the
 # function specification, add it to this test
-metadata = video_metadata_from_id("KswZ1wnCejs")
+metadata = video_metadata_from_id("KswZ1wnCejs", YOUTUBE_CLIENT)
+expected = "Intuition for Limits"
+actual = metadata["title"]
+assert actual == expected, f"Expected metadata[\"title\"] to be {expected}, but got {actual}."
+
 expected = "KswZ1wnCejs"
 actual = metadata["id"]
 assert actual == expected, f"Expected metadata[\"id\"] to be {expected}, but got {actual}."
@@ -26,37 +35,47 @@ expected = "99"
 actual = metadata["views"]
 assert int(actual) >= int(expected), f"Expected metadata[\"views\"] to be at least {expected}, but got {actual}."
 
-expected = "UC2YE5AUXk8KCGPLOfY9eCXA"
-actual = metadata["channel"]
-assert actual == expected, f"Expected metadata[\"channel\"] to be {expected}, but got {actual}."
+expected = "7"
+actual = metadata["likes"]
+assert int(actual) >= int(expected), f"Expected metadata[\"likes\"] to be at least {expected}, but got {actual}."
 
-expected = "Intuition for Limits"
-actual = metadata["title"]
-assert actual == expected, f"Expected metadata[\"title\"] to be {expected}, but got {actual}."
+expected = "Raulz Math World"
+actual = metadata["channel_name"]
+assert actual == expected, f"Expected metadata[\"channel_name\"] to be {expected}, but got {actual}."
+
+expected = "UC2YE5AUXk8KCGPLOfY9eCXA"
+actual = metadata["channel_id"]
+assert actual == expected, f"Expected metadata[\"channel_id\"] to be {expected}, but got {actual}."
 
 
 
 # Tests the video_transcript_from_id function
 transcript = video_transcript_from_id("KswZ1wnCejs")
-for word in transcript:
-    for char in " \n\t\r":
-        assert char not in word, f"Expected no whitespace in words, but found whitespace in {word}."
-assert transcript[0] == "Hey", "Expected first word in transcript to be \"Hey\""
-assert transcript[-1] == "want.", "Expected last word in transcript to be \"want.\""
+assert transcript[:3] == "Hey", "Expected first word in transcript to be \"Hey\""
+assert transcript[-5:] == "want.", "Expected last word in transcript to be \"want.\""
+
+
+
+# DO MANUAL TEST OF video_comments_from_id FUNCTION WHEN TESTING store_playlist_videos_metadata
 
 
 
 # Tests the store_playlist_video_metadata function
-store_playlist_videos_metadata("PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG", "data_scraper/")
-with open("data_scraper/PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG 0.txt", "r") as file:
+store_playlist_videos_metadata("PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG", YOUTUBE_CLIENT, "data_scraper/")
+with open("data_scraper/PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG 000.txt", "r") as file:
+    assert file.readline()[:-1] == "PLF8mMTQil4Z8vsWfZsb0bcD51Z6JoHPzG", "Expected correct playlist ID."
+    assert file.readline()[:-1] == "0", "Expected correct video playlist position."
+    assert file.readline()[:-1] == "4", "Expected correct playlist length."
+    assert file.readline()[:-1] == "Intuition for Limits", "Expected correct video title."
     assert file.readline()[:-1] == "KswZ1wnCejs", "Expected correct video ID."
     assert file.readline()[:-1] == "239", "Expected correct video duration."
     assert int(file.readline()[:-1]) >= 99, "Expected correct view count."
+    assert int(file.readline()[:-1]) >= 7, "Expected correct like count."
+    assert file.readline()[:-1] == "Raulz Math World", "Expected correct channel name."
     assert file.readline()[:-1] == "UC2YE5AUXk8KCGPLOfY9eCXA", "Expected correct channel ID."
-    assert file.readline()[:-1] == "Intuition for Limits", "Expected correct video title."
+    assert file.readline()[:3] == "Hey", "Expected correct transcript."
 # Manual Testing Strategy:
-# Check lines after the metadata tested above to see if they match the transcript
-# from the video
+# Check the transcript and comments to see if they are correct/reasonable
 
 
 print("All tests passed!")
